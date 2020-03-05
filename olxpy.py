75085b59eab35443062c24b4d78af89edf494027
João Vitor Alencar Amorim <joaoalencaramorim@gmail.com>
2020-03-05 11:23:28 -0300

    Incremento da função para scraping da página

--- olxpy.py
+++ olxpy.py
@@ -4,10 +4,10 @@ from bs4 import BeautifulSoup
 import csv
 
 url = 'https://pe.olx.com.br/grande-recife/imoveis?o='
-n_pag = 2
+quant_pag = 2 # int(input('Quantas páginas serão mineradas? '))
 
 # FUNÇÃO QUE COLETARÁ OS LINKS DE ACESSO AS PÁGINAS DOS ANÚNCIOS DE IMÓVEIS
-def Scraping_olx_v01(url, n_pag):
+def ScraperLinks(url, quant_pag):
 
 	# Abrindo arquivo de relatório sobre o processamento do scraping
 	arq  = open('relatorio', 'w')
@@ -15,7 +15,7 @@ def Scraping_olx_v01(url, n_pag):
 	# Abrindo arquivo csv para armazenar os links dos anúncios
 	file = open('links.csv', 'w')
 
-	for pag in range(1, n_pag + 1, 1):
+	for pag in range(1, quant_pag + 1, 1):
 
 		site = url + str(pag)
 
@@ -62,6 +62,48 @@ def Scraping_olx_v01(url, n_pag):
 
 	arq.close()
 	file.close()
+
 	return link
 
-Scraping_olx_v01(url, n_pag)
+#links = ScraperLink(url, n_pag)
+
+
+def ScraperPage(link):
+
+	req = Request(link, headers = {'User-Agent':'Mozilla/5.0'})
+	html = urlopen(req).read()
+	soup = BeautifulSoup(html, "html.parser")
+
+	try:
+		titulo = soup.find('h1', class_='sc-bZQynM sc-45jt43-0 bJcANz').get_text()
+	except AttributeError:
+		titulo = ""
+
+	try:
+		preco = soup.find('h2', class_='sc-bZQynM sc-1wimjbb-0 dSAaHC').get_text()
+	except AttributeError:
+		preco = ""
+
+	"""try:
+					categoria = soup.find('a', class_='sc-57pm5w-0 sc-1f2ug0x-2 dBeEuJ').get_text()
+				except AttributeError:
+					categoria = ""
+			
+				try:
+					tipo = soup.find('a', class_='sc-57pm5w-0 sc-1f2ug0x-2 dBeEuJ').get_text()
+				except AttributeError:
+					tipo = ""
+			
+				try:
+					preco = soup.find('h2', class_='sc-bZQynM sc-1wimjbb-0 dSAaHC').get_text()
+				except AttributeError:
+					preco = """
+
+	print(titulo)
+	print(preco)
+
+
+links = ['https://pe.olx.com.br/grande-recife/imoveis/apartamentos-em-prazeres-jaboatao-dos-guararapes-724925921', 'https://pe.olx.com.br/grande-recife/imoveis/ref-433-aptos-em-pau-amarelo-pe-724926028']
+
+for link in links:
+	ScraperPage(link)
